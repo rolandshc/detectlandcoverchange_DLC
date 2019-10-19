1af1ad46c943b0b3efcab07b7094c16782cac42f
durandalsowner <rolandshum.shc@gmail.com>
2019-10-19 15:36:54 +0300

    Commit Change

--- startbootstrap-stylish-portfolio-gh-pages/js/my.js
+++ startbootstrap-stylish-portfolio-gh-pages/js/my.js
@@ -1,10 +1,21 @@
+$('div .checkbox').click(function () {
+                 checkedState = $(this).attr('checked');
+                  $(this).parent('div').children('.checkbox:checked').each(function () {
+                      $(this).attr('checked', false);
+                  });
+                  $(this).attr('checked', checkedState);
+});
+
+
 function hello(){
     alert("Hello, World!");
 }
 counter = 0
 function test_toggle(){
-    var img_list = ['img/ee-2000.PNG','img/ee-2005.PNG','img/ee-2015.PNG'] 
+    var img_list = ['img/ee-2000.PNG','img/ee-2005.PNG','img/ee-2015.PNG']
     var img_number = ['2000','2005','2015']
+    var yearFrom = document.getElementById('yearFrom').values
+    var yearTo = document.getElementById('yearTo').values
     document.getElementById('main-img').src=img_list[counter % img_list.length]
     document.getElementById('main-number').innerHTML=img_number[counter % img_list.length]
     counter++
